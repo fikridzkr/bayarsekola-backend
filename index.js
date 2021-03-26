09b3640d8c31ac904b8d32b2effab98fded42708
fikridzkr <fikridzakir179@gmail.com>
2021-03-26 15:32:27 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "bayarsekola",
+});
