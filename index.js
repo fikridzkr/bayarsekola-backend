0bd8ffe26c298b5813d93376aec3bd4d742c3939
fikridzkr <fikridzakir179@gmail.com>
2021-03-27 15:59:32 +0700

    Hash password

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
 const app = express();
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 // init
 app.use(cors());
 app.use(express.json());
 
+// db connection
 const db = mysql.createConnection({
   user: "root",
   host: "localhost",
@@ -14,25 +16,33 @@ const db = mysql.createConnection({
   database: "bayarsekola",
 });
 
+// create user
 app.post("/register", (req, res) => {
   const fullName = req.body.fullName;
   const username = req.body.username;
   const email = req.body.email;
   const password = req.body.password;
   const level = req.body.level;
-  db.query(
-    "INSERT INTO users (fullname,username,email,password,level) VALUES (?,?,?,?,?)",
-    [fullName, username, email, password, level],
-    function (err, res) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("data insert", res);
-      }
+
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) {
+      console.log(err);
     }
-  );
+    db.query(
+      "INSERT INTO users (fullname,username,email,password,level) VALUES (?,?,?,?,?)",
+      [fullName, username, email, hash, level],
+      function (err, res) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("data insert", res);
+        }
+      }
+    );
+  });
 });
 
+// server
 app.listen(3001),
   () => {
     console.log("server running");
