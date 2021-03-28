1dc36cc5343e1e08f5a914dbccb8a9eae39c8c1e
fikridzkr <fikridzakir179@gmail.com>
2021-03-28 13:42:51 +0700

    Create Cache,Session

--- index.js
+++ index.js
@@ -4,10 +4,32 @@ const cors = require("cors");
 const app = express();
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
-// init
-app.use(cors());
-app.use(express.json());
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
+// init
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    key: "userId",
+    secret: "bayarsekola",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 60 * 24,
+    },
+  })
+);
 // db connection
 const db = mysql.createConnection({
   user: "root",
@@ -42,8 +64,45 @@ app.post("/register", (req, res) => {
   });
 });
 
+app.get("/login", (req, res) => {
+  if (req.session.user) {
+    res.send({ loggedIn: true, user: req.session.user });
+  } else {
+    res.send({ loggedIn: false });
+  }
+});
+
+// login
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  db.query(
+    "SELECT * FROM users WHERE username = ? ;",
+    username,
+    (err, result) => {
+      if (err) {
+        res.send({ err: err });
+      }
+      if (result.length > 0) {
+        bcrypt.compare(password, result[0].password, (error, response) => {
+          if (response) {
+            req.session.user = result;
+            console.log(req.session.user);
+            res.send(result);
+          } else {
+            res.send({ message: "Wrong username/password combination" });
+          }
+        });
+      } else {
+        res.send({ message: "User doesn't exist" });
+      }
+    }
+  );
+});
+
 // server
 app.listen(3001),
   () => {
-    console.log("server running");
+    console.log("running server");
   };
