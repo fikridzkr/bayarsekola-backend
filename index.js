64a8004eb080d71e1fa6a461b616c7d12ae8d340
fikridzkr <fikridzakir179@gmail.com>
2021-04-02 21:45:28 +0700

    Create Backend register step 2

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const saltRounds = 10;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
-
+const multer = require("multer");
+const upload = multer();
+const fs = require("fs");
+const { promisify } = require("util");
+const pipeline = promisify(require("stream").pipeline);
 // init
 app.use(express.json());
 app.use(
@@ -26,11 +30,11 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      expires: new Date(Date.now() + 30000),
-      maxAge: 30000,
+      expires: 3600000,
     },
   })
 );
+
 // db connection
 const db = mysql.createConnection({
   user: "root",
@@ -41,19 +45,19 @@ const db = mysql.createConnection({
 
 // create user
 app.post("/register", (req, res) => {
-  const fullName = req.body.fullName;
   const username = req.body.username;
   const email = req.body.email;
   const password = req.body.password;
   const level = req.body.level;
+  const is_active = req.body.is_active;
 
   bcrypt.hash(password, saltRounds, (err, hash) => {
     if (err) {
       console.log(err);
     }
     db.query(
-      "INSERT INTO users (fullname,username,email,password,level) VALUES (?,?,?,?,?)",
-      [fullName, username, email, hash, level],
+      "INSERT INTO users (username,email,password,level,is_active) VALUES (?,?,?,?,?)",
+      [username, email, hash, level, is_active],
       function (err, res) {
         if (err) {
           console.log(err);
@@ -65,6 +69,7 @@ app.post("/register", (req, res) => {
   });
 });
 
+// login get
 app.get("/login", (req, res) => {
   if (req.session.user) {
     res.send({ loggedIn: true, user: req.session.user });
@@ -101,6 +106,74 @@ app.post("/login", (req, res) => {
     }
   );
 });
+
+// kelas
+app.get("/kelas", (req, res) => {
+  db.query("SELECT * FROM kelas", (err, response) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send({ kelas: response });
+    console.log(response);
+  });
+});
+
+app.post(
+  "/datastudent",
+  upload.single("foto"),
+  async function (req, res, next) {
+    console.log(req.body);
+    const {
+      file,
+      body: { name },
+    } = req;
+    if (!req.file) {
+      console.log("foto tidak ada");
+    }
+    if (file.detectedFileExtension != ".jpg")
+      next(new Error("Invalid File Type"));
+    const is_active = req.body.is_active;
+    const user_id = req.body.user_id;
+    const fotoName =
+      Math.floor(Math.random() * 100000).toString() +
+      file.detectedFileExtension;
+    const nis = req.body.nis;
+    const nama = req.body.nama;
+    const kelas = req.body.kelas;
+    const jurusan = req.body.jurusan;
+    const jenis_kelamin = req.body.jenis_kelamin;
+    await pipeline(
+      file.stream,
+      fs.createWriteStream(`${__dirname}/public/images/${fotoName}`)
+    );
+    res.send("File uploaded as " + fotoName);
+
+    // insert data siswa
+    db.query(
+      "INSERT INTO siswa (user_id,foto,nis,nama,id_kelas,jurusan,jenis_kelamin) VALUES (?,?,?,?,?,?,?)",
+      [user_id, fotoName, nis, nama, kelas, jurusan, jenis_kelamin],
+      function (err, res) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("data insert: ", res);
+        }
+      }
+    );
+
+    // update active user
+    db.query(
+      `UPDATE users SET is_Active = '${is_active}' WHERE id = '${user_id}'`,
+      function (err, res) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("data update: ", res);
+        }
+      }
+    );
+  }
+);
 
 // server
 app.listen(3001),
